07d33d2f3bbcb6c44a09e737e44546111322b714
demonicirfan <hashtag.irfan@gmail.com>
2021-07-25 14:12:43 +0530

    variables and data types

---
+++ Basics/hello.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt" //format package
+func main() {
+	var nameOne string = "mario"
+	var nameTwo = "luig"
+	var nameThree string
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameOne = "peach"
+	nameThree = "browser"
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameour := "yoshi"
+	fmt.Println(nameour)
+
+	//ints
+	var ageOne int = 20
+	var ageTwo = 30
+	agethree := 40
+	fmt.Println(ageOne, ageTwo, agethree)
+}
