3e363af5a43d283137f1e2f5e5fd14064cbbee31
irfan asif <hashtag.irfan@gmail.com>
2021-08-01 07:37:56 +0530

    taking varous innputs

--- Basics/takingInput.go
+++ Basics/takingInput.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -12,13 +14,19 @@ func main() {
 	// fmt.Scanln(&myString)
 	// fmt.Println(myString)
 
-	var name string = "irfan"
-	var a, _ = fmt.Println(name)
-	fmt.Println(a)
+	// var name string = "irfan"
+	// var a, _ = fmt.Println(name)
+	// fmt.Println(a)
+
+	// reader := bufio.NewReader(os.Stdin)
+	// fmt.Print("enter your full name: ")
+	// myname, _ := reader.ReadString('\n')
+	// fmt.Println(myname)
 
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("enter your full name: ")
-	myname, _ := reader.ReadString('\n')
-	fmt.Println(myname)
+	fmt.Print("enter your rating: ")
+	myrating, _ := reader.ReadString('\n')
+	mynumRating, _ := strconv.ParseFloat(strings.TrimSpace(myrating), 64)
+	fmt.Println(mynumRating + 2)
 
 }
