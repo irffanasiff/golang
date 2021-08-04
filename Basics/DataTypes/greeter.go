cd9063fe7ea3cdd82dedfb07474a9053a6d6c135
demonicirfan <hashtag.irfan@gmail.com>
2021-08-04 18:19:39 +0530

    printf

--- Basics/DataTypes/greeter.go
+++ Basics/DataTypes/greeter.go
@@ -8,7 +8,7 @@ import (
 func main() {
 
 	fmt.Printf("%#v\n", os.Args)
-
+	
 	fmt.Println("path: ", os.Args[0])
 	fmt.Println("Args[1]: ", os.Args[1])
 	fmt.Println("Args[2]: ", os.Args[2],
---
+++ Basics/printf/formatSpecifiers.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		planet   = "venus"
+		distance = 261
+		orbital  = 224.701
+		hasLife  = false
+	)
+
+	fmt.Printf("Planet: %s\n", planet)
+	fmt.Printf("Distance: %d millions kms\n", distance)
+	fmt.Printf("Orbital Period: %f days\n", orbital)
+	fmt.Printf("Does %s has life? %t\n",
+		planet, hasLife)
+
+	fmt.Printf("%v is %v away. Think! %[2]v kms! %[1]v OMG!\n",
+		planet, distance)
+
+	fmt.Printf("Orbital Period: %f days\n", orbital)
+	fmt.Printf("Orbital Period: %.0f days\n", orbital)
+	fmt.Printf("Orbital Period: %.1f days\n", orbital)
+	fmt.Printf("Orbital Period: %.3f days\n", orbital)
+	fmt.Printf("Orbital Period: %.4f days\n", orbital)
+	
+	
+}
